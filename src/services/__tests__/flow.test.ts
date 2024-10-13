3d829adb6aaf8e7eb3e15248bd76d3d90b6d2f91
Austin Kline <austindavidkline@gmail.com>
2024-10-12 17:58:31 -0700

    add method to get network from an address

--- src/services/__tests__/flow.test.ts
+++ src/services/__tests__/flow.test.ts
@@ -1,5 +1,5 @@
 import { jest } from '@jest/globals';
-import { configure, getAccountSummary } from '../flow';
+import { configure, getAccountSummary, networkFromAddress } from '../flow';
 import * as fcl from '@onflow/fcl';
 import { Balance } from '@/types/flow';
 
@@ -17,12 +17,29 @@ describe('flow service', () => {
     describe('getAccountSummary', () => {
         it('should return account summary for valid address', async () => {
             const result = await getAccountSummary(emulatorServiceAccountAddress);
-            
+
             expect(result).not.toBeNull()
             expect(Number(result?.flowAvailableBalance)).toBeGreaterThan(9999)
 
             const flowTokenBalance = result?.balances?.find((balance: Balance) => balance.vaultType.includes("FlowToken"))
             expect(Number(flowTokenBalance?.vaultBalance)).toBeGreaterThan(9999)
+        });
+    });
+
+    describe('networkFromAddress', () => {
+        it('should match emulator address', () => {
+            const network = networkFromAddress(emulatorServiceAccountAddress);
+            expect(network).toBe("emulator");
+        });
+        
+        it('should match testnet address', () => {
+            const network = networkFromAddress("0x40387cea622425a3");
+            expect(network).toBe("testnet");
+        });
+
+        it('should match mainet address', () => {
+            const network = networkFromAddress("0x455eb332c23a23e8");
+            expect(network).toBe("mainnet");
         });
     });
 });
--- src/services/flow.ts
+++ src/services/flow.ts
@@ -4,6 +4,7 @@ import { AccountSummary, Balance } from '@/types/flow';
 
 import * as fcl from '@onflow/fcl';
 import { TransactionStatus } from '@onflow/typedefs';
+import { Truculenta } from 'next/font/google';
 
 export const configure = (network: string) => {
     console.log("configuring flow", { network })
@@ -107,3 +108,64 @@ export const waitForSeal = async (txnId: string): Promise<TransactionStatus> => 
     const result = await fcl.tx(txnId).onceSealed()
     return result
 }
+
+export const networkFromAddress = (address: string): string => {
+    // Explanation:
+    // 1. Remove the '0x' prefix from the address using slice(2)
+    // 2. Create a new Uint8Array of length 8
+    // 3. Iterate 8 times, each time taking 2 characters from the address
+    // 4. Parse these 2 characters as a hexadecimal number and store it in the array
+    const withoutPrefix = address.slice(2);
+    const uint8Array = new Uint8Array(8);
+    for (let i = 0; i < 8; i++) {
+        // The 16 here specifies the radix (base) for parsing the string.
+        uint8Array[i] = parseInt(withoutPrefix.slice(i * 2, i * 2 + 2), 16);
+    }
+
+    // Next, convert this array of 8 bytes into a number
+    const addressNumber = uint8Array.reduce((acc, byte) => (acc << 8n) | BigInt(byte), 0n);
+    for (const network in networkCodeWords) {
+        if (isNumberInNetwork(addressNumber, network)) {
+            return network;
+        }
+    }
+
+    return "";
+}
+
+const isNumberInNetwork = (number: bigint, network: string): boolean => {
+    var parity = 0n;
+    var codeword: bigint = networkCodeWords[network];
+    codeword = codeword ^ number;
+    if (codeword === 0n) {
+        return false;
+    }
+
+    for (let i = 0; i < parityCheckMatrix.length; i++) {
+        const column = parityCheckMatrix[i];
+        if((codeword & 1n) === 1n) {
+            parity = parity ^ BigInt(column);
+        }
+        codeword = codeword >> 1n;
+    }
+
+    return parity === 0n && codeword === 0n;
+}
+
+
+const networkCodeWords: Record<string, bigint> = {
+    mainnet: 0n,
+    testnet: 0x6834ba37b3980209n,
+    emulator: 0x1cb159857af02018n
+}
+
+const parityCheckMatrix = [
+    0x00001, 0x00002, 0x00004, 0x00008, 0x00010, 0x00020, 0x00040, 0x00080,
+    0x00100, 0x00200, 0x00400, 0x00800, 0x01000, 0x02000, 0x04000, 0x08000,
+    0x10000, 0x20000, 0x40000, 0x7328d, 0x6689a, 0x6112f, 0x6084b, 0x433fd,
+    0x42aab, 0x41951, 0x233ce, 0x22a81, 0x21948, 0x1ef60, 0x1deca, 0x1c639,
+    0x1bdd8, 0x1a535, 0x194ac, 0x18c46, 0x1632b, 0x1529b, 0x14a43, 0x13184,
+    0x12942, 0x118c1, 0x0f812, 0x0e027, 0x0d00e, 0x0c83c, 0x0b01d, 0x0a831,
+    0x0982b, 0x07034, 0x0682a, 0x05819, 0x03807, 0x007d2, 0x00727, 0x0068e,
+    0x0067c, 0x0059d, 0x004eb, 0x003b4, 0x0036a, 0x002d9, 0x001c7, 0x0003f
+]
